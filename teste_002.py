60d65812058369eba9e5a9a693a6167c076ffaa0
João Ornellas <65920201+jo4o0rn3ll4s@users.noreply.github.com>
2023-01-26 19:42:21 -0300

    Delete teste_002.py

--- teste_002.py
+++
@@ -1,6 +0,0 @@
-rep = int(input('Escreve o numero de repetições desejadas: '))
-
-for r in range(1,rep+1, 1): #começo, fim, incremento(apenas numero) ou decremento(- o numero)
-   
-    print('Irei repetir essa frase {} vezes'.format(rep))
-    print('Já escrevi {} vezes'.format(r))
