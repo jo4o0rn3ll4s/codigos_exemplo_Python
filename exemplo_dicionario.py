6a428e1e5f71cc21f698a33f2b8035672265d93e
João Ornellas <65920201+jo4o0rn3ll4s@users.noreply.github.com>
2023-02-02 19:42:23 -0300

    Create exemplo_dicionario.py

---
+++ exemplo_dicionario.py
@@ -0,0 +1,11 @@
+val = ('val0':'valor0','val1':'valor1'...)
+val = dict(val0 = 'valor0',val1 = 'valor1'...)
+
+#Retorna os valores do dicionario
+val.values()
+
+#Retorna as keys do dicionario
+val.keys()
+
+#Retorna os itens do dicionario
+val.items()
