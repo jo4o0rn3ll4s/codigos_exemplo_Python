3df9fc8b6ec2b7f7e2fce38af18b862aeab13370
João Ornellas <65920201+jo4o0rn3ll4s@users.noreply.github.com>
2023-02-02 19:21:58 -0300

    Create exemplo_lista.py

---
+++ exemplo_lista.py
@@ -0,0 +1,18 @@
+val = ['val0','val1'...]
+val = list(('val0','val1'...))
+
+#adiciona um valor ao final da lista
+val.append(valor)
+val.append('val2')
+
+#adiciona um valor no indice especifico
+val.insert(indice, valor)
+val.insert(2, 'val2')
+
+#retira um valor do indice especifico
+val.pop(indice)
+val.pop(2)
+
+#retira um valor pelo próprio valor
+val.remove(valor)
+val.remove('val2')
