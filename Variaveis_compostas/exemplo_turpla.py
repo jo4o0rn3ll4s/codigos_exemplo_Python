0a479779bfbf1b73e5382a4a894b47adb8bd27a5
João Ornellas <65920201+jo4o0rn3ll4s@users.noreply.github.com>
2023-02-02 20:40:45 -0300

    Create exemplo_turpla.py

---
+++ Variaveis_compostas/exemplo_turpla.py
@@ -0,0 +1,11 @@
+val = ('val0','val1'...)
+val = tuple(('val0','val1'...))
+
+#retorna o numero de vezes que o elemento
+#aparece na turpla
+val.count(valor)
+val.count('val1')
+
+#retorna o indice do elemento pesquisado
+val.index(valor)
+val.index('val0')
