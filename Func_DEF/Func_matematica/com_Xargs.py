e0ebe84f5531e0262a549806f58058dc6e85c4e9
João Ornellas <65920201+jo4o0rn3ll4s@users.noreply.github.com>
2023-02-08 13:56:12 -0300

    Update com_Xargs.py

--- Func_DEF/Func_matematica/com_Xargs.py
+++ Func_DEF/Func_matematica/com_Xargs.py
@@ -1,23 +1,42 @@
 def soma(*val):
+    """
+    :val-> os valores que desejam somar
+    :retorna-> a soma de todos os elementos descomprimidos de *val
+    """
     resul = 0
     for i in val:
         resul += i
     return resul
 
-def sub(*val):
-    resul = 0
+
+def sub(total=0, *val):
+    """
+    :total-> Valor que será subtraido pelo *val
+    :val-> os valores de substradores
+    :retorna-> a subtração de todos os elementos descomprimidos de *val
+    """
     for i in val:
-        resul -= i
-    return resul
+        total -= i
+    return total
+
 
 def mult(*val):
-    resul = 0
+    """
+    :val-> os valores que desejam multiplicar
+    :retorna-> o multiplo de todos os elementos descomprimidos de *val
+    """
+    resul = 1
     for i in val:
         resul *= i
     return resul
 
-def div(*val):
-    resul = 0
+
+def div(total=0, *val):
+    """
+    :total-> Valor que será dividido pelo *val
+    :val-> os valores de divisão
+    :retorna-> o valor dividido por todos os elementos descomprimidos de *val
+    """
     for i in val:
-        resul /= i
-    return resul
+        total /= i
+    return total
