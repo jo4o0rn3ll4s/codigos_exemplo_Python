e14e79e06bde8d9fc5fe7732a363ea3ca4d68d2d
João Ornellas <65920201+jo4o0rn3ll4s@users.noreply.github.com>
2023-02-08 10:59:13 -0300

    Update and rename a to Func_matematica.py

---
+++ Func_DEF/Func_matematica.py
@@ -0,0 +1,15 @@
+def soma(val1, val2):
+    resul = val1 + val2
+    return resul
+
+def sub(val1, val2):
+    resul = val1 + val2
+    return resul
+
+def mult(val1, val2):
+    resul = val1 + val2
+    return resul
+
+def div(val1, val2):
+    resul = val1/val2
+    return resul
