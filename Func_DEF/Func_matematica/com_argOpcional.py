8e98d03007003512e20c140949615e87803d7f28
João Ornellas <65920201+jo4o0rn3ll4s@users.noreply.github.com>
2023-02-09 10:37:49 -0300

    Create com_argOpcional.py

---
+++ Func_DEF/Func_matematica/com_argOpcional.py
@@ -0,0 +1,15 @@
+def soma(val1 = 0, val2 = 0, val3 = 0):
+    resul = val1 + val2 + val3
+    return resul
+
+def sub(val1 = 0, val2 = 0, val3 = 0):
+    resul = val1 - val2 - val3
+    return resul
+
+def mult(val1 = 1, val2 = 1, val3 = 1):
+    resul = val1 * val2 * val3
+    return resul
+
+def div(val1 = 1, val2 = 1, val3 = 1):
+    resul = (val1/val2)/val3
+    return resul
