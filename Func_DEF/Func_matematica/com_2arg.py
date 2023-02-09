237a432558112f8acb77880c8873bfb5c75d7fe3
João Ornellas <65920201+jo4o0rn3ll4s@users.noreply.github.com>
2023-02-09 10:30:41 -0300

    Update com_2arg.py

--- Func_DEF/Func_matematica/com_2arg.py
+++ Func_DEF/Func_matematica/com_2arg.py
@@ -3,11 +3,11 @@ def soma(val1, val2):
     return resul
 
 def sub(val1, val2):
-    resul = val1 + val2
+    resul = val1 - val2
     return resul
 
 def mult(val1, val2):
-    resul = val1 + val2
+    resul = val1 * val2
     return resul
 
 def div(val1, val2):
